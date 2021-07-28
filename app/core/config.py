fe36c342732a966a048257ade5d7182fdfde6a9c
Ivan <ivan.mjasoyedov@gmail.com>
2021-07-28 14:24:32 +0300

    added class Settings for database connection

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic import (
+    BaseSettings,
+    PostgresDsn
+)
+
+
+class Settings(BaseSettings):
+    postgres_database_url: PostgresDsn
+
+    class Config:
+        env_file: str = ".env"
+
+
+settings = Settings()
