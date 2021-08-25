8298e13cc63e7bc6b224aa9f2b8de8b5482f6f72
Ivan <ivan.mjasoyedov@gmail.com>
2021-08-25 18:06:38 +0300

    testing pre-commit black formatter git actions hook

--- app/db/models/todo.py
+++ app/db/models/todo.py
@@ -1,20 +1,17 @@
 from datetime import datetime
 
-from sqlalchemy import (
-    Table, Column,
-    Integer, String, Text, DateTime
-)
+from sqlalchemy import Table, Column, Integer, String, Text, DateTime
 
 from app.db.base import metadata
 
 
 todos = Table(
-    'todos',
+    "todos",
     metadata,
-    Column('id', Integer, autoincrement=True, primary_key=True),
-    Column('title', String(100), nullable=False),
-    Column('order', Integer, nullable=False),
-    Column('description', Text),
-    Column('created_at', DateTime, default=datetime.now()),
-    Column('updated_at', DateTime, default=datetime.now(), onupdate=datetime.now())
+    Column("id", Integer, autoincrement=True, primary_key=True),
+    Column("title", String(100), nullable=False),
+    Column("order", Integer, nullable=False),
+    Column("description", Text),
+    Column("created_at", DateTime, default=datetime.now()),
+    Column("updated_at", DateTime, default=datetime.now(), onupdate=datetime.now()),
 )
