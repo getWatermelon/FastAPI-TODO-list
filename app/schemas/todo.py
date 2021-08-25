f1e470ca67275887d9380f2c22e52d6f525bbea9
Ivan <ivan.mjasoyedov@gmail.com>
2021-08-25 18:08:27 +0300

    testing pre-commit black formatter git actions hook

--- app/schemas/todo.py
+++ app/schemas/todo.py
@@ -14,8 +14,13 @@ class Todo(BaseModel):
 
 
 class TodoIn(BaseModel):
-    title: str = Field(..., min_length=1,
-                       description="title length must be at least 1 character")
-    order: int = Field(..., gt=0, lt=1000,
-                       description="Task order number must be more than 0 and less than 1000")
+    title: str = Field(
+        ..., min_length=1, description="title length must be at least 1 character"
+    )
+    order: int = Field(
+        ...,
+        gt=0,
+        lt=1000,
+        description="Task order number must be more than 0 and less than 1000",
+    )
     description: Optional[str]
